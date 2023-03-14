7d7a136de4bb5480146c52a786ff770eb26f97f7
Tahir Yasin <tahiryasin123@gmail.com>
2023-03-14 10:59:20 +0400

    Arabic translations added

---
+++ src/Tahiryasin/Reports/src/Resources/lang/ar/app.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'layouts' => [
+
+        'left-menu' => [
+
+            'reports' => 'التقارير',
+            'sales-report' => 'تقرير المبيعات',
+
+        ],
+    ],
+
+    'reports' => [
+        'sales-report' => 'تقرير المبيعات',
+        'invoice-report' => 'تقرير الفواتير',
+        'inventory-report' => 'تقرير المخزونات',
+        'shipping-report' => 'تقرير الشحن',
+        'refunds-report' => 'تقرير المبالغ المستردة',
+        'tax-report' => 'تقرير الضرائب',
+        'inventory_date_hint' => 'تاريخ إنشاء المنتج',
+        'from' => 'من',
+        'to' => 'إلى',
+        'generate' => 'إنشاء',
+    ]
+];
